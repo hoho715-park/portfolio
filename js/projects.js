2069bc8a799efbdb3723b39c975b3cdaf0a8810d
박성호 <andy2001@g.hongik.ac.kr>
2026-08-06 12:27:05 +0900

    guider 세담터 최우수상 추가

--- js/projects.js
+++ js/projects.js
@@ -727,6 +727,7 @@ function setupButtonHandlers() {
           console.log("📸 Opening award images...");
           window.openImageModal([
             "images/project/award/guider_award_1.png",
+            "images/project/award/guider_award_2.png",
           ]);
         } else if (action === "certificate") {
           console.log("🎓 Opening certificate images...");
